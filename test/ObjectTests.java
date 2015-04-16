05328808a7a35c37dd62225d9e8930f6b5baa333
Jake <Drost011@umn.edu>
2015-04-16 15:43:28 -0500

    Consolidated tests into one file

--- test/ObjectTests.java
+++ test/ObjectTests.java
@@ -1,3 +1,4 @@
+import data.CraigslistUrls;
 import objects.Post;
 import objects.Search;
 import org.jsoup.Jsoup;
@@ -9,10 +10,23 @@ import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
-public class PostTests {
+public class ObjectTests {
 
     @Test
-    public void constructorTest() throws IOException {
+    public void searchValidURLsTest() {
+        Search search1 = new Search(CraigslistUrls.ALL.owner(), "minneapolis", null);
+        Search search2 = new Search(CraigslistUrls.ALL.owner(), "minneapolis", "Xbox One -games");
+        Search search3 = new Search(CraigslistUrls.ALL.owner(), "minneapolis", "Xbox One -games", 100, -1);
+        Search search4 = new Search(CraigslistUrls.ALL.owner(), "minneapolis", "Xbox One -games", 100, 200);
+
+        assertEquals(search1.searchUrl(1), "http://minneapolis.craigslist.org/search/sso?s=0");
+        assertEquals(search2.searchUrl(2), "http://minneapolis.craigslist.org/search/sso?s=100&query=Xbox%20One%20-games");
+        assertEquals(search3.searchUrl(3), "http://minneapolis.craigslist.org/search/sso?minAsk=100&s=200&query=Xbox%20One%20-games");
+        assertEquals(search4.searchUrl(4), "http://minneapolis.craigslist.org/search/sso?minAsk=100&maxAsk=200&s=300&query=Xbox%20One%20-games");
+    }
+
+    @Test
+    public void postConstrutorTest() throws IOException {
         Post curPost;
         Search dummySearch = new Search("cat", "city", "match", -1, -1);
         String dir = System.getProperty("user.dir") + "\\test\\resources";
